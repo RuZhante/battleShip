2c6c5cb5fe3c2ae4e8d21703572024875d52a7f3
Rustem Zhantemirov <zhantemirov.rustem@gmail.com>
2022-05-22 21:07:16 +0600

    Create get_random_coordinates method

--- src/game.rb
+++ src/game.rb
@@ -1,5 +1,6 @@
 require_relative 'board.rb'
 require_relative 'print.rb'
+require_relative 'ship.rb'
 
 # create the game_board
 game_board = Board.new
@@ -7,3 +8,6 @@ game_board.build_game_board
 
 # print the game board
 Print.print_game_board(game_board.board)
+
+# create ships
+Ship.build_ship(game_board.board, 1, 4)
--- src/ship.rb
+++ src/ship.rb
@@ -1,10 +1,24 @@
 class Ship
-  def initialize
+
+  attr_accessor :get_random_coordinates
+
+  def self.build_ship(board, count_ship, count_palub)
+    coordinates = get_random_coordinates
+    puts coordinates
 
   end
 
-  def build_ship(count_ship, count_palub)
-
+  def self.get_random_coordinates
+    row = rand(1..10)
+    column = rand(1..10)
+    coordinates = {row: row, column: column}
+    return coordinates
   end
+
+  def self.is_available_coordinates?(board, coordinates)
+    result = board.find { |item|  item.include?(coordinates)}
+  end
+
+  private_class_method :get_random_coordinates ,:is_available_coordinates?
 
 end
