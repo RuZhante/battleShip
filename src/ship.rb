c3b3118eb2fd266b49d7de5cff6442e2f657c5f4
Rustem Zhantemirov <zhantemirov.rustem@gmail.com>
2022-06-02 22:47:39 +0600

    feat(ship.rb): create check_ship_contact

--- src/ship.rb
+++ src/ship.rb
@@ -5,14 +5,18 @@ class Ship
   def self.build_ship(board, count_ship, count_palub)
     available_coordinates_board = Marshal.load(Marshal.dump(board))
     count_ship.times do
-      count_palub.times do
+      coordinates = get_random_coordinates(available_coordinates_board)
+      find_coordinates = check_ship_contact(available_coordinates_board, coordinates)
+      while find_coordinates.size < 4
         coordinates = get_random_coordinates(available_coordinates_board)
-        set_palub(coordinates, board)
+        find_coordinates = check_ship_contact(available_coordinates_board, coordinates)
       end
+      build_coordinates = get_ship_building_direction(available_coordinates_board, coordinates, count_palub)
+      while build_coordinates.size.zero?
+        build_coordinates = get_ship_building_direction(available_coordinates_board, coordinates, count_palub)
+      end
+      set_palub(coordinates, board, build_coordinates)
     end
-
-    puts('--->', board.size)
-    puts('--->', available_coordinates_board.size)
   end
 
   def self.get_random_coordinates(available_coordinates_board)
@@ -20,18 +24,33 @@ class Ship
     available_coordinates_board.delete_at(random_index)
   end
 
-  def self.set_palub(coordinates, board)
+  def self.check_ship_contact(available_coordinates_board, coordinates)
+    available_coordinates_board & [{ row: coordinates[:row] + 1, column: coordinates[:column] - 1, status: '.' },
+                                   { row: coordinates[:row] - 1, column: coordinates[:column] - 1,
+                                     status: '.' },
+                                   { row: coordinates[:row] + 1, column: coordinates[:column] + 1,
+                                     status: '.' },
+                                   { row: coordinates[:row] - 1, column: coordinates[:column] + 1,
+                                     status: '.' }]
+  end
+
+  def self.get_ship_building_direction(available_coordinates_board, coordinates, count_palub)
+    n = count_palub - 1
+    available_coordinates_board & [{ row: coordinates[:row], column: coordinates[:column] - n,
+                                     status: '.' },
+                                   { row: coordinates[:row], column: coordinates[:column] + n,
+                                     status: '.' },
+                                   { row: coordinates[:row], column: coordinates[:column] - n,
+                                     status: '.' },
+                                   { row: coordinates[:row], column: coordinates[:column] - n,
+                                     status: '.' }]
+  end
+
+  def self.set_palub(_coordinates, board, _count_palub, _build_coordinates)
     board.map do |i|
-      i[:status] = 'H' if i[:row] == coordinates[:row] && i[:column] == coordinates[:column]
+      puts i # todo
     end
   end
 
-  def self.available_coordinates?(board, coordinates); end
-
-  def self.ship_building_direction
-    direction = %w[left right up down]
-    direction.sample
-  end
-
-  private_class_method :get_random_coordinates, :available_coordinates?, :ship_building_direction
+  private_class_method :get_random_coordinates
 end
